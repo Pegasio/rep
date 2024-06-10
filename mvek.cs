d58c5a9019a4b2efe697ff8e052741bfaa6d9caf
Pegasio <109171075+Pegasio@users.noreply.github.com>
2024-06-10 13:33:28 +0400

    2

--- mvek.cs
+++ mvek.cs
@@ -1,12 +1,14 @@
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
@@ -17,34 +19,62 @@ using Telegram.Bot.Types.ReplyMarkups;
 
 class Program
 {
+    static readonly List<long> ActiveChatIds = new List<long>();
+    static readonly Dictionary<long, DateTime> LastMessageTimes = new Dictionary<long, DateTime>();
 
-    static async Task<Task> HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    static readonly int MinMessageIntervalSeconds = 1;
+
+    static string selectedCourse = null;
+    static string spreadsheetId;
+    static string selectedSheet = null;
+    static string groupName;
+    static TelegramBotClient botClient;
+    static DateTime botStartTime;
+
+    static async Task<List<string>> GetSpreadsheetSheets(string spreadsheetId)
+    {
+        GoogleCredential credential;
+        using (var stream = new FileStream("key.json", FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream)
+                .CreateScoped(new[] { SheetsService.Scope.SpreadsheetsReadonly });
+        }
+
+        var service = new SheetsService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "Google Sheets API Sample",
+        });
+
+        var spreadsheet = await service.Spreadsheets.Get(spreadsheetId).ExecuteAsync();
+        var sheets = spreadsheet.Sheets.Select(sheet => sheet.Properties.Title).ToList();
+        return sheets;
+    }
+
+    static async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
         var apiRequestException = exception as ApiRequestException;
         if (apiRequestException != null && apiRequestException.ErrorCode == 403)
         {
-            // Бот был удален из группы, игнорируем эту ошибку
             Console.WriteLine("Бот был удален из группы.");
         }
         else
         {
-            // Обработка остальных ошибок
             Console.WriteLine($"Telegram API Error:\n[{apiRequestException?.ErrorCode}]\n{exception.Message}");
         }
-
-        // Продолжаем работу
-        return Task.CompletedTask;
     }
+
     static async Task Main(string[] args)
     {
-        var botClient = new TelegramBotClient("7086897352:AAGI-Fd_O5mnb-w4IR5W-fnwIGGvRpXmoRo");
+        botClient = new TelegramBotClient("7086897352:AAGI-Fd_O5mnb-w4IR5W-fnwIGGvRpXmoRo");
+
+        botStartTime = DateTime.UtcNow;
 
         using CancellationTokenSource cts = new();
 
-        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
         ReceiverOptions receiverOptions = new()
         {
-            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+            AllowedUpdates = Array.Empty<UpdateType>()
         };
 
         botClient.StartReceiving(
@@ -59,75 +89,195 @@ class Program
         Console.WriteLine($"Start listening for @{me.Username}");
         Console.ReadLine();
 
-        // Send cancellation request to stop bot
         cts.Cancel();
     }
 
-    static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, System.Threading.CancellationToken cancellationToken)
+    static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
         if (update.Message is not { } message)
             return;
 
-        if (message.Type == MessageType.ChatMemberLeft && message.LeftChatMember.Id == botClient.BotId)
+        var chatId = message.Chat.Id;
+
+        // Проверяем, было ли это сообщение от пользователя.
+        if (message.From == null)
         {
-            // Бот был удален из чата, проигнорируем это событие
-            Console.WriteLine($"Бот был удален из чата {message.Chat.Id}");
+            Console.WriteLine("Не удалось получить информацию о пользователе.");
+            return;
         }
 
-        var chatId = message.Chat.Id;
+        var userId = message.From.Id;
+
+        // Проверяем, если пользователь уже взаимодействовал с ботом, используя его ID.
+        if (!ActiveChatIds.Contains(userId))
+        {
+            ActiveChatIds.Add(userId);
+        }
+
+        if (message.Date.ToUniversalTime() < botStartTime)
+        {
+            Console.WriteLine($"Сообщение от пользователя {chatId} проигнорировано, так как было отправлено до запуска бота.");
+            return;
+        }
+
+        if (LastMessageTimes.TryGetValue(chatId, out DateTime lastMessageTime))
+        {
+            if ((DateTime.UtcNow - lastMessageTime).TotalSeconds < MinMessageIntervalSeconds)
+            {
+                Console.WriteLine($"Сообщение от пользователя {chatId} проигнорировано из-за частого отправления.");
+                return;
+            }
+        }
+
+        Console.WriteLine($"Получено сообщение от пользователя {chatId}: {message.Text}");
+
+        LastMessageTimes[chatId] = DateTime.UtcNow;
 
         if (message.Text == "/start")
         {
-            // Создаем клавиатуру с кнопками дней недели
             var replyMarkup = new ReplyKeyboardMarkup(new[]
             {
             new[]
             {
-                new KeyboardButton("Понедельник"),
-                new KeyboardButton("Вторник"),
-                new KeyboardButton("Среда")
+                new KeyboardButton("Курс 1"),
+                new KeyboardButton("Курс 2"),
             },
             new[]
             {
-                new KeyboardButton("Четверг"),
-                new KeyboardButton("Пятница"),
-                new KeyboardButton("Суббота")
+                new KeyboardButton("Курс 3"),
+                new KeyboardButton("Курс 4"),
             }
-        });
+        })
+            {
+                OneTimeKeyboard = true,
+                ResizeKeyboard = true
+            };
 
             await botClient.SendTextMessageAsync(
                 chatId: chatId,
-                text: "Выберите день недели:",
+                text: "Выберите курс:",
                 replyMarkup: replyMarkup,
                 cancellationToken: cancellationToken
             );
         }
         else if (message.Text != null)
         {
-            string dayOfWeekInput = message.Text.ToLower(); // Приводим текст сообщения к нижнему регистру для удобства сравнения
+            // Проверяем, является ли сообщение текстом "Курс n".
+            string text = message.Text.ToLower();
 
-// Проверяем, содержит ли сообщение ключевые слова (дни недели)
-            if (dayOfWeekInput == "понедельник"  || dayOfWeekInput == "вторник" ||  dayOfWeekInput == "среда" ||
-                dayOfWeekInput == "четверг"  || dayOfWeekInput == "пятница" ||  dayOfWeekInput == "суббота")
+            if (text.StartsWith("курс "))
             {
-                // Вызываем метод для получения расписания
-                var schedule = await GetScheduleFromGoogleSheets("ДИС-204.121.Б", dayOfWeekInput);
+                int course;
+                if (int.TryParse(text.Substring(5), out course) && course >= 1 && course <= 4)
+                {
+                    selectedCourse = $"курс {course}";
+                    switch (course)
+                    {
+                        case 1:
+                            spreadsheetId = "1fPHNrnMDighH92ZkGN3GcNfZ5abo2IU1a12adLBqXj8";
+                            break;
+                        case 2:
+                            spreadsheetId = "13aLnayHCB53L48H4KedZkHYntdCApkb7rv0gFkx3XjY";
+                            break;
+                        case 3:
+                            spreadsheetId = "1E-ieb-3K4GwIBGXgiiy-koA2AEtxF9InGRW80wtAO7A";
+                            break;
+                        case 4:
+                            spreadsheetId = "1r541rAXYzV9FO14zaUNbpQ00CXkc1aAWIrLd-RuP7uQ";
+                            break;
+                    }
 
-                // Отправляем расписание пользователю
+                    // Запускаем окно выбора группы после выбора курса.
+                    await OfferSheetSelection(botClient, chatId, cancellationToken);
+                }
+            }
+            else if (await IsSheetName(text))
+            {
+                selectedSheet = text;
+                groupName = selectedSheet;
+
+                var replyMarkup = new ReplyKeyboardMarkup(new[]
+                {
+                new[]
+                {
+                    new KeyboardButton("Понедельник"),
+                    new KeyboardButton("Вторник"),
+                },
+                new[]
+                {
+                    new KeyboardButton("Среда"),
+                    new KeyboardButton("Четверг"),
+                },
+                new[]
+                {
+                    new KeyboardButton("Пятница"),
+                    new KeyboardButton("Суббота"),
+                }
+            })
+                {
+                    OneTimeKeyboard = true,
+                    ResizeKeyboard = true
+                };
+
                 await botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: schedule,
+                    text: "Выберите день недели:",
+                    replyMarkup: replyMarkup,
                     cancellationToken: cancellationToken
                 );
+            }
+            else
+            {
+                string dayOfWeekInput = text;
+                if (dayOfWeekInput == "понедельник" || dayOfWeekInput == "вторник" || dayOfWeekInput == "среда" ||
+                    dayOfWeekInput == "четверг" || dayOfWeekInput == "пятница" || dayOfWeekInput == "суббота")
+                {
+                    var schedule = await GetScheduleFromGoogleSheets(groupName, dayOfWeekInput);
+
+                    var hideKeyboard = new ReplyKeyboardMarkup(new KeyboardButton[][] { });
+
+                    await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: schedule,
+                        replyMarkup: hideKeyboard,
+                        cancellationToken: cancellationToken
+                    );
+                }
             }
         }
     }
 
 
+    static async Task<bool> IsSheetName(string text)
+    {
+        var sheets = await GetSpreadsheetSheets(spreadsheetId);
+        return sheets.Contains(text, StringComparer.OrdinalIgnoreCase);
+    }
+
+    static async Task OfferSheetSelection(ITelegramBotClient botClient, long chatId, CancellationToken cancellationToken)
+    {
+        var sheets = await GetSpreadsheetSheets(spreadsheetId);
+
+        var buttons = sheets.Select(sheet =>
+            new KeyboardButton[] { new KeyboardButton(sheet) }
+        ).ToArray();
+
+        var replyMarkup = new ReplyKeyboardMarkup(buttons)
+        {
+            OneTimeKeyboard = true,
+            ResizeKeyboard = true
+        };
+
+        await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Выберите Группу:",
+            replyMarkup: replyMarkup,
+            cancellationToken: cancellationToken
+        );
+    }
+
     static async Task<string> GetScheduleFromGoogleSheets(string groupName, string dayOfWeekInput)
     {
-        // Ваш код для получения расписания из Google Sheets
-        string spreadsheetId = "1r541rAXYzV9FO14zaUNbpQ00CXkc1aAWIrLd-RuP7uQ";
         string range = $"{groupName}!A:G";
 
         GoogleCredential credential;
@@ -143,7 +293,6 @@ class Program
             ApplicationName = "Google Sheets API Sample",
         });
 
-        // Вычисляем ближайший будущий день недели
         DayOfWeek requestedDayOfWeek = ConvertRussianDayOfWeekToEnum(dayOfWeekInput);
         if (requestedDayOfWeek != DayOfWeek.Sunday)
         {
@@ -153,25 +302,19 @@ class Program
             DateTime requestedDate = currentDate.AddDays(daysUntilRequestedDay);
             Console.WriteLine($"The requested date is: {requestedDate:dd.MM.yyyy}");
 
-
-            // Используем запрашиваемую дату вместо текущей даты
             var request = service.Spreadsheets.Values.Get(spreadsheetId, range);
             var response = await request.ExecuteAsync();
             var values = response.Values;
 
             if (values != null && values.Count > 0)
             {
-                // Ваш код для обработки данных из Google Sheets
                 StringBuilder scheduleBuilder = new StringBuilder();
                 bool found = false;
-
-
 
                 for (int i = 0; i < values.Count; i++)
                 {
                     var row = values[i];
 
-                    // Отладочная информация для каждой строки
                     Console.WriteLine($"Processing row {i}: {string.Join(", ", row)}");
 
                     if (row.Count >= 4 && row[0].ToString().ToLower() == dayOfWeekInput.ToLower() && DateTime.TryParseExact(row[1].ToString(), "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime rowDate))
@@ -179,7 +322,6 @@ class Program
                         Console.WriteLine($"Checking date: {rowDate:dd.MM.yyyy} against {requestedDate:dd.MM.yyyy}");
                         if (rowDate.Date == requestedDate.Date)
                         {
-                            // Найдена подходящая строка, обрабатываем ее
                             scheduleBuilder.AppendLine($"Расписание на {rowDate:dd.MM.yyyy} ({dayOfWeekInput}):");
 
                             for (int j = i; j < Math.Min(values.Count, i + 8); j++)
@@ -188,18 +330,17 @@ class Program
 
                                 if (currentRow.Count >= 7)
                                 {
-                                    Console.WriteLine($"nhin{j}: {string.Join(", ", currentRow)}");
+                                    Console.WriteLine($"Row {j}: {string.Join(", ", currentRow)}");
                                     scheduleBuilder.AppendLine($"{currentRow[4]}:");
                                     scheduleBuilder.AppendLine($"Время: {currentRow[3]}");
-                                    scheduleBuilder.AppendLine($"Аудитория: {currentRow[6]}");
                                     scheduleBuilder.AppendLine($"Преподаватель: {currentRow[5]}");
+                                    scheduleBuilder.AppendLine($"Аудитория: {currentRow[6]}");
                                     scheduleBuilder.AppendLine();
                                     found = true;
-
                                 }
                             }
 
-                            break; // выход из цикла, так как мы нашли нужную дату и обработали ее
+                            break;
                         }
                     }
                 }
@@ -210,53 +351,32 @@ class Program
                 }
                 else
                 {
-                    return $"No schedule found for {requestedDate:dd.MM.yyyy} ({dayOfWeekInput}).";
+                    return $"Расписание на {requestedDate:dd.MM.yyyy} ({dayOfWeekInput}) не найдено.";
                 }
             }
             else
             {
-                return $"No schedule found for group {groupName}.";
+                return $"Расписание для {groupName} не найдено.";
             }
-
-
-
         }
         else
         {
-            return "Invalid day of the week.";
+            return "Неверный день недели.";
         }
     }
 
     static DayOfWeek ConvertRussianDayOfWeekToEnum(string dayOfWeek)
     {
-        switch (dayOfWeek.ToLower())
+        return dayOfWeek.ToLower() switch
         {
-            case "понедельник":
-                return DayOfWeek.Monday;
-            case "вторник":
-                return DayOfWeek.Tuesday;
-            case "среда":
-                return DayOfWeek.Wednesday;
-            case "четверг":
-                return DayOfWeek.Thursday;
-            case "пятница":
-                return DayOfWeek.Friday;
-            case "суббота":
-                return DayOfWeek.Saturday;
-            case "воскресенье":
-                return DayOfWeek.Sunday;
-            default:
-                throw new ArgumentException("Invalid day of the week", nameof(dayOfWeek));
-        }
+            "понедельник" => DayOfWeek.Monday,
+            "вторник" => DayOfWeek.Tuesday,
+            "среда" => DayOfWeek.Wednesday,
+            "четверг" => DayOfWeek.Thursday,
+            "пятница" => DayOfWeek.Friday,
+            "суббота" => DayOfWeek.Saturday,
+            "воскресенье" => DayOfWeek.Sunday,
+            _ => throw new ArgumentException("Неверный день недели", nameof(dayOfWeek)),
+        };
     }
-
-
-
-
-
-
-
-
-
-
 }
